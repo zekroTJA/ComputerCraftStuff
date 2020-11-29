666a6d0415e260dde78b723d6b22fc4d5da80190
zekro <riho@live.de>
2020-11-29 21:33:39 +0100

    Update checker.lua

--- CPHAIEN/1.0/checker.lua
+++ CPHAIEN/1.0/checker.lua
@@ -1,72 +1,29 @@
---[[
+local REDNET_SIDE = "top"
+local CHECK_TIMEOUT = 5
 
-© 2020 Ringo Hofmmann (zekro Development)
-
-CPHAIEN STORAGE SYSTEM - CHECKER
-
----------
-
-CPHAIEN PROTOCOL V1.0
-
-The CPHAIEN protocol is a simple protocol which is utilized for
-communication between the CLIENT pc's, SERVER pc and the CHECKER
-interface pc.
-
-The protocol communication is baed on a simple command-response
-communication system.
-
-A command is a string encoded map which MUUST always have the
-key "cmd" with the COMMAND as string value. These are the 
-currently implemented commands:
-
-"order" / 1
-
-    Issues an ORDER to a CLIENT.
-    Takes one argument named "n", which specifies the ammount 
-    of ordered entities (type number).
-    An order request MUST be answered by an acknowledgement
-    message with the encoded content < "ack" >.
-
-    Example:
-        A -> B:  {"cmd"="order","n"=5,}
-        B -> A:  "ack"
-
-"check" / 0
-
-    Requests the CLIENT state.
-    Takes no arguments.
-    A check command must be responded by an encoded STATUS 
-    string. A status can be any string, but the string "ok"
-    MUST be interpreted as the normal, default state.
-
-    Example:
-        A -> B:  {"cmd"="check",}
-        B -> A:  "ok"
-
-]]
-
-local REDNET_SIDE = "left"
-local CHECK_TIMEOUT = 60
+local ID_TABLE = {
+    [2] = 69,
+    [3] = 88,
+}
 
 ----------------------------------------------------
 
-function clear()
+local rednetSide = nil
+
+local function clear()
     shell.run("clear")
 end
 
-function open()
-    if not rednet.isOpen(REDNET_SIDE) then
-        rednet.open(REDNET_SIDE)
-    end
+local function open()
+    rednet.open(REDNET_SIDE)
+    rednetSide = REDNET_SIDE
 end
 
-function close()
-    if rednet.isOpen(REDNET_SIDE) then
-        rednet.close(REDNET_SIDE)
-    end
+local function close()
+    rednet.close(rednetSide)
 end
 
-function broadcast()
+local function broadcast()
     open()
     local data = textutils.serialize({
         ["cmd"] = "check",
@@ -74,7 +31,7 @@ function broadcast()
     return rednet.broadcast(data)
 end
 
-function receive(timeout)
+local function receive(timeout)
     open()
     local peer, data = rednet.receive(timeout)
     if not peer then
@@ -84,29 +41,45 @@ function receive(timeout)
     return peer, msg
 end
 
+local function itoa(i)
+    if i < 10 then
+        return "0" .. i
+    end
+    return tostring(i)
+end
+
 ----------------------------------------------------
 
-function receivejob(timeout)
+local statusTable = {}
+
+local function receivejob(timeout)
+    for k, v in pairs(ID_TABLE) do
+        statusTable[k] = "timeout"
+    end
+
     while true do
         peer, msg = receive(timeout)
         if not peer then
+            clear()
+            print("Starting checking for status issues ...\n")
+            for k, v in pairs(statusTable) do
+                local id = ID_TABLE[k]
+                if id then
+                    print(itoa(id) .. " - " .. v)
+                end
+            end
             return
         end
-        if msg ~= "ok" then
-            print("CID " .. peer .. " has status " .. msg)
-        end
+        statusTable[peer] = msg
     end
 end
 
-function bcloop()
-    -- sleep(5)
+local function bcloop()
     broadcast()
     receivejob(CHECK_TIMEOUT)
 end
 
 clear()
--- job = coroutine.create(receivejob)
--- coroutine.resume(job)
 
 print("Starting checking for status issues ...")
 while not bcloop() do end
