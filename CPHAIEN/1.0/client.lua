afbbc7e6f0aec55a0103d8b3c15bca529ee5ead9
zekro <riho@live.de>
2020-11-29 20:56:44 +0100

    Update client.lua

--- CPHAIEN/1.0/client.lua
+++ CPHAIEN/1.0/client.lua
@@ -1,78 +1,35 @@
---[[
-
-© 2020 Ringo Hofmmann (zekro Development)
-
-CPHAIEN STORAGE SYSTEM - CLIENT
-
----------
-
-CPHAIEN PROTOCOL V1.0
-
-The CPHAIEN protocol is a simple protocol which is utilized for
-communication between the CLIENT pc's, SERVER pc and the CHECKER
-interface pc.
-
-The protocol communication is baed on a simple command-response
-communication system.
-
-A command is a string encoded map which MUUST always have the
-key "cmd" with the COMMAND as string value. These are the 
-currently implemented commands:
-
-"order" / 1
-
-    Issues an ORDER to a CLIENT.
-    Takes one argument named "n", which specifies the ammount 
-    of ordered entities (type number).
-    An order request MUST be answered by an acknowledgement
-    message with the encoded content < "ack" >.
-
-    Example:
-        A -> B:  {"cmd"="order","n"=5,}
-        B -> A:  "ack"
-
-"check" / 0
-
-    Requests the CLIENT state.
-    Takes no arguments.
-    A check command must be responded by an encoded STATUS 
-    string. A status can be any string, but the string "ok"
-    MUST be interpreted as the normal, default state.
-
-    Example:
-        A -> B:  {"cmd"="check",}
-        B -> A:  "ok"
-
-]]
-
-local REDNET_SIDE = "right"
-local REDSTONE_SIDE = "back"
+local ORDER_SIGNAL_SIDE = "back"
 
 ----------------------------------------------------
 
-function clear()
+local rednetSide = nil
+
+local function clear()
     shell.run("clear")
 end
 
-function open()
-    if not rednet.isOpen(REDNET_SIDE) then
-        rednet.open(REDNET_SIDE)
+local function open()
+    for _, v in pairs(redstone.getSides()) do
+        if pcall(rednet.open, v) then
+            rednetSide = v
+            return
+        end
     end
+    print("ERR: Could not detect any wireless modem.")
+    exit()
 end
 
-function close()
-    if rednet.isOpen(REDNET_SIDE) then
-        rednet.close(REDNET_SIDE)
-    end
+local function close()
+    rednet.close(rednetSide)
 end
 
-function send(receiver, msg)
+local function send(receiver, msg)
     open()
     local data = textutils.serialize(msg)
     return rednet.send(receiver, data)
 end
 
-function receive()
+local function receive()
     open()
     local peer, data = rednet.receive()
     if not peer then
@@ -84,27 +41,18 @@ end
 
 ----------------------------------------------------
 
-function order(peer, n)
+local function order(peer, n)
     print("Received order ammount " .. n .. " from cid " .. peer)
     send(peer, "ack")
     for i = 1, n, 1 do
-        redstone.setOutput(REDSTONE_SIDE, true)
+        redstone.setOutput(ORDER_SIGNAL_SIDE, true)
         sleep(0.25)
-        redstone.setOutput(REDSTONE_SIDE, false)
+        redstone.setOutput(ORDER_SIGNAL_SIDE, false)
         sleep(0.5)
     end
 end
 
-function check(peer)
-    print("Received check from cid " .. peer)
-    local state = "ok"
-    if redstone.getInput(REDSTONE_SIDE) then
-        state = "blocked"
-    end
-    send(peer, state)
-end
-
-function mainloop()
+local function mainloop()
     local peer, msg = receive()
     if not peer then
         return
@@ -112,8 +60,6 @@ function mainloop()
 
     if msg["cmd"] == "order" then
         order(peer, msg["n"])
-    elseif msg["cmd"] == "check" then
-        check(peer)
     end
 end
 
